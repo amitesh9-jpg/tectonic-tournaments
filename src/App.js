a78da5ac098d6954030b0633b64a96a98c201b9b
Amitesh Nath <nathamiteshr@gmail.com>
2025-06-08 17:36:11 +0530

    Remove Contact page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import RegistrationForm from "./components/RegistrationForm";
-import Contact from './pages/Contact'; // Capital C
+
 function Home() {
   return (
     <section className="min-h-screen flex items-center justify-center bg-black text-white text-3xl">
@@ -18,7 +18,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<RegistrationForm />} />
-        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
